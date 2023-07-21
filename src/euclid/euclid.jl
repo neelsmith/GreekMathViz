096a3171b8ed2b32415cb95fe96f696bf2c9730d
Neel Smith <dnsmith.neel@gmail.com>
2023-07-21 09:21:46 -0400

    constrain URNs

--- src/euclid/euclid.jl
+++ src/euclid/euclid.jl
@@ -7,10 +7,16 @@ end
 
 
 function readmath(u, Euclid; format = :luxor)
+    @info("Read Euclid for $(u)")
+    @info("Its versionid is $(versionid(u))")
     if format != :luxor 
         msg = "`readmath`: Only `:luxor` format currently supported."
         throw(ArgumentError(msg))
-        
+
+    elseif workcomponent(u) != "tlg1799.tlg001.luxor"
+        @warn("`readmath`: can only read mathematical contents of Euclid's *Geometry* identified by URNs with work component `tlg1799.tlg001.luxor`.")
+        nothing
+
     else
         println("Hooray! We're reading Euclid!")
     end
--- test/test_dispatch.jl
+++ test/test_dispatch.jl
@@ -10,4 +10,8 @@
     euc_def1_1 = CtsUrn("urn:cts:greekLit:tlg1799.tlg001.luxor:1.def.1")
 
     @test_throws ArgumentError readmath(euc_def1_1, format = :nope_not_this)
+
+    wrong_version = CtsUrn("urn:cts:greekLit:tlg1799.tlg001.NOT_LUXOR:1.def.1")
+    @test  readmath(wrong_version) |> isnothing
+
 end
